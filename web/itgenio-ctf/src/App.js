5a18c3c1449539a4ad9bac46a1fc14eeff135beb
Makar Baderko <makarbaderko@gmail.com>
2020-06-08 08:50:43 +0300

    Added about

--- web/itgenio-ctf/src/App.js
+++ web/itgenio-ctf/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import Header from './components/header/header.component';
+import Home_Page from './pages/home/home.component';
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <Home_Page></Home_Page>
     </div>
   );
 }
--- web/itgenio-ctf/src/components/header/header.component.jsx
+++ web/itgenio-ctf/src/components/header/header.component.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import './header.styles.css'
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+  } from "react-router-dom";  
 const Header = () => (
-    <div className="Header">
-        <img id="logo"src="https://static.tildacdn.com/tild3062-3136-4266-b732-336262616631/logo2.png" alt="Logo"></img>
-        <a id="sign-up">SIGN UP OR LOGIN</a>
-    </div>
+    <Router>
+        <div className="Header">
+            <Link to="/"><img id="logo"src="https://static.tildacdn.com/tild3062-3136-4266-b732-336262616631/logo2.png" alt="Logo"></img></Link>
+            <Link to="/sign-in-or-up"id="sign-up">SIGN UP OR LOGIN</Link>
+        </div>
+    </Router>
     
 );
 
