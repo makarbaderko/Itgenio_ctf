97c2d9dd9a95bc5d484e65e186d50765ef5cc8e1
Makar Baderko <makarbaderko@gmail.com>
2020-06-08 08:21:40 +0300

    Added header

---
+++ web/itgenio-ctf/src/components/header/header.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './header.styles.css'
+const Header = () => (
+    <div className="Header">
+        <img id="logo"src="https://static.tildacdn.com/tild3062-3136-4266-b732-336262616631/logo2.png" alt="Logo"></img>
+        <a id="sign-up">SIGN UP OR LOGIN</a>
+    </div>
+    
+);
+
+export default Header;
