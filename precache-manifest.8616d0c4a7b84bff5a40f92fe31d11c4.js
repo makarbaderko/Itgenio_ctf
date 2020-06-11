35cd57cfe02b20d01201d5123b09a83fd06b5c01
Makar Baderko <makarbaderko@gmail.com>
2020-06-11 18:37:52 +0300

    Updates

---
+++ precache-manifest.8616d0c4a7b84bff5a40f92fe31d11c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e547fd300d551d45fa5b1d7157b03d1",
+    "url": "/itgenio-ctf/index.html"
+  },
+  {
+    "revision": "039c1c183c296ada9052",
+    "url": "/itgenio-ctf/static/css/main.994dcba4.chunk.css"
+  },
+  {
+    "revision": "95409bcb1b9d10b8947b",
+    "url": "/itgenio-ctf/static/js/2.e255e827.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/itgenio-ctf/static/js/2.e255e827.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "039c1c183c296ada9052",
+    "url": "/itgenio-ctf/static/js/main.d55f0467.chunk.js"
+  },
+  {
+    "revision": "c0ff46a0704e68aeb9f9",
+    "url": "/itgenio-ctf/static/js/runtime-main.8ad265d7.js"
+  }
+]);
